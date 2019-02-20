441ac2dd03e43d31bec324de3d8dd5ec38a9688d
craigmc08 <craigmc08@gmail.com>
2019-02-20 18:39:35 -0500

    Implement render methods in Scene using Task

--- raymarcher/Scene.hpp
+++ raymarcher/Scene.hpp
@@ -2,10 +2,15 @@
 #define SCENE_H_
 
 #include <stdlib.h>
+#include <stdio.h>
+#include <vector>
+#include <thread>
+#include <chrono>
 
 #include "Vector.hpp"
 #include "Camera.hpp"
 #include "SceneObject.hpp"
+#include "Task.hpp"
 
 class Scene {
 private:
@@ -40,9 +45,42 @@ public:
         objects.push_back(object);
     }
 
+    std::vector<int> Render() {
+        std::vector<int> pixels(width * height * 3);
+
+        const unsigned n_threads = std::thread::hardware_concurrency();
+        std::vector<std::thread> threads{n_threads};
+
+        // 32 is magic render tile size. Should expose this as a setting
+        Task::setSceneSettings(32, width, height, pixels);
+        for (auto &t : threads) t = std::thread{Task()};
+
+        while (Task::threadsCompleted != n_threads) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        }
+
+        for (auto &t : threads) t.join();
+
+        return pixels;
+    }
+
     void RenderToFile(const char* filename) {
-        // TODO
-        // Should render and output to a file
+        FILE* fp;
+        if (fopen_s(&fp, filename, "wb") != 0) {
+            printf("Failed to open file %s", filename);
+        }
+
+        // Write PPM file header
+        fprintf(fp, "P6 %d %d 255\n", width, height);
+
+        auto pixels = Render();
+
+        // Write image pixels to file
+        for (int i = 0; i < width * height * 3; i++) {
+            fprintf(fp, "%c", pixels[i]);
+        }
+
+        fclose(fp);
     }
 };
 
